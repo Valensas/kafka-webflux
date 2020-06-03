4e1430ebbc4029afe06a13a3df439504fef9a069
Moray Baruh <moray@valensas.com>
2020-06-03 11:12:33 +0300

    producer: add support for publishing ProducerRecord

--- src/main/kotlin/com/valensas/common/kafka/webflux/producer/KafkaProducer.kt
+++ src/main/kotlin/com/valensas/common/kafka/webflux/producer/KafkaProducer.kt
@@ -2,12 +2,14 @@
 package com.valensas.common.kafka.webflux.producer
 
 import org.apache.kafka.clients.producer.ProducerRecord
+import org.reactivestreams.Publisher
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import reactor.kafka.sender.KafkaSender
 import reactor.kafka.sender.SenderRecord
 import reactor.kafka.sender.SenderResult
+import reactor.kotlin.core.publisher.toFlux
 
 fun <T> Flux<T>.toKafka(topic: String, producer: KafkaProducer): Flux<SenderResult<T>> =
     producer.send(topic, this)
@@ -29,6 +31,15 @@ class KafkaProducer(
         @Suppress("UNCHECKED_CAST")
         val sender = this.sender as KafkaSender<String, T>
         return sender.send(toSenderRecord(topic, data))
+    }
+
+    fun <K, V> send(record: Publisher<ProducerRecord<K, V>>): Flux<SenderResult<V>> {
+        @Suppress("UNCHECKED_CAST")
+        val sender = this.sender as KafkaSender<K, V>
+        return record
+            .toFlux()
+            .map { SenderRecord.create(it, it.value()) }
+            .let { sender.send(it) }
     }
 
     private fun <T> toSenderRecord(topic: String, flux: Flux<T>): Flux<SenderRecord<String, T, T>> =
