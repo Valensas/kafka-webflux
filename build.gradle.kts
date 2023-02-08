83d8606901e795e9b4c742fa28dbf5f1caba6dbc
Moray Baruh <moraybaruh@gmail.com>
2023-02-08 13:22:27 +0000

    add gitlab-cii

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,11 +47,17 @@ tasks.withType<Test> {
 
 publishing {
     repositories {
-        maven {
-            this.url = uri(project.property("AWS_REPO_URL").toString())
-            credentials(AwsCredentials::class) {
-                this.accessKey = project.property("AWS_REPO_USER_ACCESS_KEY").toString()
-                this.secretKey = project.property("AWS_REPO_USER_SECRET_KEY").toString()
+        if (System.getenv("CI_API_V4_URL") != null) {
+            maven {
+                name = "Gitlab"
+                url = uri("${System.getenv("CI_API_V4_URL")}/projects/${System.getenv("CI_PROJECT_ID")}/packages/maven")
+                credentials(HttpHeaderCredentials::class.java) {
+                    name = "Job-Token"
+                    value = System.getenv("CI_JOB_TOKEN")
+                }
+                authentication {
+                    create("header", HttpHeaderAuthentication::class)
+                }
             }
         }
     }
